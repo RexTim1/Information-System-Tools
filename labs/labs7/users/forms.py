982ccc58d2724643a9dab4d1bb0e1cda997ab609
RexTim1 <58368047+RexTim1@users.noreply.github.com>
2022-11-26 15:41:23 +0700

    Delete labs/labs7 directory

--- labs/labs7/users/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-# объявляем форму с именем NameForm
-# принадлежащую классу Form
-class NameForm(forms.Form):
-    # форма имеет одно поле, строкового типа
-    # имеет подпись - label
-    # имеет максимальную длину 100 символов
-    user_name = forms.CharField(label='The username contains:', max_length=100)
